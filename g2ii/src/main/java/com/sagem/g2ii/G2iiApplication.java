9c26effd137d86a5873702dc8c685989b8eb6f1f
SEMAHWECHTATI <samah.ouechtati@esprit.tn>
2026-04-03 23:47:34 +0100

    2er phase authentification

--- g2ii/src/main/java/com/sagem/g2ii/G2iiApplication.java
+++ g2ii/src/main/java/com/sagem/g2ii/G2iiApplication.java
@@ -2,8 +2,10 @@ package com.sagem.g2ii;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 @SpringBootApplication
+@EntityScan(basePackages = "com.sagem.g2ii.Entity")
 public class G2iiApplication {
 
 	public static void main(String[] args) {
